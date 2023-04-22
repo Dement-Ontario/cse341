90162f68b4d1867c59d1ea6844f7ad0529e262b7
Joseph T <jdtoronto999@outlook.com>
2023-04-22 17:39:48 +0100

    console log from 3000 to port

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = 3000;
 app.use('/', require('./routes'));
 
 app.listen(process.env.port || port);
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
+console.log('Web Server is listening at port '+ (process.env.port || port));
